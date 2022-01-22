8d18bb88201594805df21e122ba75b6389c1e977
Your Name <kewin.f@aeturnum.com>
2022-01-22 13:26:44 +0530

    first commit

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger'
+
+export class User {
+
+    @ApiProperty()
+    id: number
+
+    @ApiProperty()
+    name: string
+
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [
+    { id: 1, name: 'Kewin' },
+    { id: 2, name: 'Marlon' },
+    { id: 3, name: 'Pravin' },
+    { id: 4, name: 'Vivian' },
+    { id: 5, name: 'Adrian' },
+  ];
+
+  findAll(name: string) {
+    if (name) {
+      return this.users.filter(user => user.name === name)
+    }
+    return this.users;
+  }
+
+  findById(userId: number) {
+    return this.users.find((obj) => obj.id === userId);
+  }
+
+  createUser(createUserDto: CreateUserDto): User {
+    const newUser = { id: Date.now(), ...createUserDto };
+    this.users.push(newUser);
+    return newUser
+  }
+}
